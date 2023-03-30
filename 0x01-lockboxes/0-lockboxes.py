b4a751dcbcf6b8cb20f2e26d7fc7cd997f8f58a7
Cayvoh-K <muhiakevin12@gmail.com>
2023-03-30 08:32:12 +0000

    lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -3,16 +3,14 @@
 
 
 def canUnlockAll(boxes):
-    """Create a list to keep track of opened boxes"""
-    opened_boxes = [False] * len(boxes)
-    opened_boxes[0] = True
+    """This function will take a list of lists and the content
+    of a list will unlock other lists"""
 
-    available_keys = boxes[0]
-
-    while available_keys:
-        key = available_keys.pop(0)
-        if not opened_boxes[key]:
-            opened_boxes[key] = True
-            available_keys.extend(boxes[key])
-
-    return all(opened_boxes)
+    keys = [0]
+    for key in keys:
+        for boxKey in boxes[key]:
+            if boxKey not in keys and boxKey < len(boxes):
+                keys.append(boxkey)
+    if len(keys) == len(boxes):
+        return True
+    return False
