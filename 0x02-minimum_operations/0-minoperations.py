f33f17ebf5a45cbd8a0acfe9a9461de2e2ed1127
Cayvoh-K <muhiakevin12@gmail.com>
2023-04-06 06:47:15 +0000

    minimum operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -5,6 +5,11 @@ method that calculates the least number of operations needed in a character
 
 
 def minOperations(n):
+    """
+    A function that calculates the fewest number of operations
+    needed to give a result of exactly n H characters in a file
+    """
+
     if n < 1:
         return 0
     elif n == 1:
