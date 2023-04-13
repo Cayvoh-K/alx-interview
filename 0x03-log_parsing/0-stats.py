80e63ae6a72d9df136b68fb4d756831e8ae394db
Cayvoh-K <muhiakevin12@gmail.com>
2023-04-13 20:12:11 +0000

    log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,42 +1,38 @@
 #!/usr/bin/python3
-""" a script that reads stdin line by line and computes metric"""
-
+"""
+a script that reads stdin line and compute metrics
+"""
 
 import sys
-from collections import defaultdict
 
-total_file_size = 0
-status_code_counts = defaultdict(int)
-line_count = 0
-
+cache = {'200': 0, '301': 0, '400': 0, '401': 0,
+         '403': 0, '404': 0, '405': 0, '500': 0}
+total_size = 0
+counter = 0
 
 try:
     for line in sys.stdin:
-        line = line.strip()
-        # parse the line
-        try:
-            ip_address, date, request, status_code, file_size = line.split()
-            if request != "GET /projects/260 HTTP/1.1":
-                continue
-            status_code = int(status_code)
-            file_size = int(file_size)
-        except ValueError:
-            continue
+        line_list = line.split("")
+        if len(line_list) > 4:
+            code = line_list[-2]
+            size = int(line_list[-1])
+            if code in cache.keys():
+                cache[code] += 1
+            total_size += size
+            counter += 1
 
-        # update metrics
-        total_file_size += file_size
-        status_code_counts[status_code] += 1
-        line_count += 1
+        if counter == 10:
+            counter = 0
+            print('File size: {}'.format(total_size))
+            for key, value in sorted(cache.items()):
+                if value != 0:
+                    print('{}: {}'.format(key, value))
 
-        # print metrics every 10 lines
-        if line_count % 10 == 0:
-            print(f'Total file size: {total_file_size}')
-            for status_code in sorted(status_code_counts.keys()):
-                print(f'{status_code}: {status_code_counts[status_code]}')
-            print()
+except Exception as err:
+    pass
 
-except KeyboardInterrupt:
-    # print final metrics on keyboard interrupt
-    print(f'Total file size: {total_file_size}')
-    for status_code in sorted(status_code_counts.keys()):
-        print(f'{status_code}: {status_code_counts[status_code]}')
+finally:
+    print('File size: {}'.format(total_size))
+    for key, value in sorted(cache.items()):
+        if value != 0:
+            print('{}: {}'.format(key, value))
