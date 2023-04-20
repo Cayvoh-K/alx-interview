fdc5d4a3126f2c822cc4277bd22f9c3da4b7107b
Cayvoh-K <muhiakevin12@gmail.com>
2023-04-20 21:35:32 +0000

    utf8

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -2,18 +2,22 @@
 """
 UTF-8 Validation
 """
-def validUTF8(data):
 
-for byte in data:
-    if num_bytes == 0:
-        if (byte >> 5) == 0b110:
-            num_bytes = 1
-        elif (byte >> 4) == 0b110:
-            num_bytes = 2
-        elif (byte >> 3) == 0b11110:
-            num_bytes = 3
-        elif (byte >> 7):
-            return False
+
+def validUTF8(data):
+    # Number of bytes in the current UTF-8 character
+    num_bytes = 0
+
+    for byte in data:
+        if num_bytes == 0:
+            if (byte >> 5) == 0b110:
+                num_bytes = 1
+            elif (byte >> 4) == 0b110:
+                num_bytes = 2
+            elif (byte >> 3) == 0b11110:
+                num_bytes = 3
+            elif (byte >> 7):
+                return False
     else:
         if (byte >> 6) != 0b10:
             return False
