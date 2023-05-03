d92667687b14de940a7e6b53272d5a59bf0f67e4
Cayvoh-K <muhiakevin12@gmail.com>
2023-05-03 17:16:36 +0000

    Strar Wars characters

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,42 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const movieId = process.argv[2];
+
+if (!movieId) {
+  console.error('Error: Please specify a movie ID as the first argument');
+  process.exit(1);
+}
+
+const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
+
+request(apiUrl, { json: true }, (err, res, body) => {
+  if (err) {
+    console.error(`Error: ${err.message}`);
+    process.exit(1);
+  }
+
+  if (res.statusCode !== 200) {
+    console.error(`Error: API returned status code ${res.statusCode}`);
+    process.exit(1);
+  }
+
+  const characters = body.characters;
+
+  characters.forEach(characterUrl => {
+    request(characterUrl, { json: true }, (err, res, body) => {
+      if (err) {
+        console.error(`Error: ${err.message}`);
+        return;
+      }
+
+      if (res.statusCode !== 200) {
+        console.error(`Error: API returned status code ${res.statusCode}`);
+        return;
+      }
+
+      console.log(body.name);
+    });
+  });
+});
