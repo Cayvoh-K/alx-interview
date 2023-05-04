1de8c1b8dcf53e011c8bc0985285fbd205d806ab
Cayvoh-K <muhiakevin12@gmail.com>
2023-05-04 14:13:19 -0700

    starwars

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,40 +3,51 @@
 const request = require('request');
 
 const movieId = process.argv[2];
-
-if (!movieId) {
-  console.error('Error: Please specify a movie ID as the first argument');
-  process.exit(1);
-}
-
 const url = `https://swapi-api.hbtn.io/api/films/${movieId}`;
+let people = [];
+const names = [];
 
-request(url, { json: true }, (err, res, body) => {
-  if (err) {
-    console.error(`Error: ${err.message}`);
-    process.exit(1);
+const requestCharacters = async () => {
+  await new Promise(resolve => request(url, (err, res, body) => {
+    if (err || res.statusCode !== 200) {
+      console.error('Error: ', err, '| StatusCode: ', res.statusCode);
+    } else {
+      const jsonBody = JSON.parse(body);
+      people = jsonBody.characters;
+      resolve();
+    }
+  }));
+};
+
+const requestNames = async () => {
+  if (people.length > 0) {
+    for (const p of people) {
+      await new Promise(resolve => request(p, (err, res, body) => {
+        if (err || res.statusCode !== 200) {
+          console.error('Error: ', err, '| StatusCode: ', res.statusCode);
+        } else {
+          const jsonBody = JSON.parse(body);
+          names.push(jsonBody.name);
+          resolve();
+        }
+      }));
+    }
+  } else {
+    console.error('Error: Please specify a movie Id as the first argument');
   }
+};
 
-  if (res.statusCode !== 200) {
-    console.error(`Error: API returned status code ${res.statusCode}`);
-    process.exit(1);
+const getCharNames = async () => {
+  await requestCharacters();
+  await requestNames();
+
+  for (const n of names) {
+    if (n === names[names.length - 1]) {
+      process.stdout.write(n);
+    } else {
+      process.stdout.write(n + '\n');
+    }
   }
+};
 
-  const characters = body.characters;
-
-  characters.forEach(characterUrl => {
-    request(characterUrl, { json: true }, (err, res, body) => {
-      if (err) {
-        console.error(`Error: ${err.message}`);
-        return;
-      }
-
-      if (res.statusCode !== 200) {
-        console.error(`Error: API returned status code ${res.statusCode}`);
-        return;
-      }
-
-      console.log(body.name);
-    });
-  });
-});
+getCharNames();
