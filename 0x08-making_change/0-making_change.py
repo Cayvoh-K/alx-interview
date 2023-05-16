6cabc9b774c2d9d65995d8ad65ddb8faadddd8ab
Cayvoh-K <muhiakevin12@gmail.com>
2023-05-16 20:00:06 +0000

    making change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -8,13 +8,15 @@ def makeChange(coins, total):
     if total <= 0:
         return 0
 
-    min_coins = [float('inf')] * (total + 1)
-
+    min_coins = [total + 1] * (total + 1)
     min_coins[0] = 0
+
+
     for coin in coins:
         for i in range(coin, total + 1):
             min_coins[i] = min(min_coins[i], 1 + min_coins[i - coin])
-    if min_coins[total] == float('inf'):
+    
+    if min_coins[total] > total:
         return -1
     else:
         return min_coins[total]
