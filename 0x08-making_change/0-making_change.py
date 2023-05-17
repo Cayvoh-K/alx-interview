ec76870badb0b67750aaa3bb50dd5ca289a0cd09
Cayvoh-K <muhiakevin12@gmail.com>
2023-05-17 07:14:05 +0000

    making change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -8,14 +8,15 @@ def makeChange(coins, total):
     if total <= 0:
         return 0
 
-    min_coins = [total + 1] * (total + 1)
+    min_coins = [sys.maxsize] * (total + 1)
     min_coins[0] = 0
 
-    for coin in coins:
-        for i in range(coin, total + 1):
-            min_coins[i] = min(min_coins[i], 1 + min_coins[i - coin])
+    for i in range(1, total + 1):
+        for coin in coins:
+            if coin <= i:
+                min_coins[i] = min(min_coins[i], 1 + min_coins[i - coin])
 
-    if min_coins[total] > total:
+    if min_coins[total] == sys.maxsize:
         return -1
     else:
         return min_coins[total]
