736833663255f20dd5ba3b51411f051607fd59c7
Cayvoh-K <muhiakevin12@gmail.com>
2023-05-25 07:34:12 +0000

    island perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,21 +1,16 @@
 #!/usr/bin/python3
-"""
-function that returns the perimeter of the island
+"""function that returns the perimeter of the island.
 """
 
 
 def island_perimeter(grid):
+    """island perimeter function"""
     if not grid or not grid[0]:
         return 0
 
-    rows = len(grid)
-    cols = len(grid[0])
-
-    perimeter = 0
-
-    for i in range(rows):
-        for j in range(cols):
-            if grid[i][j] == 1:
+    for i, row in enumerate(grid):
+        for j, cell in enumerate(row):
+            if cell == 1:
                 perimeter += 4
 
                 if i > 0 and grid[i - 1][j] == 1:
